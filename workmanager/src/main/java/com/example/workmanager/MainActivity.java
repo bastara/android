b71a5798ba6fe055f88573ba52c036c168b10ac2
bastara <kolobokuu@yandex.ru>
2019-05-09 12:21:06 +0700

    workManager ready

--- workmanager/src/main/java/com/example/workmanager/MainActivity.java
+++ workmanager/src/main/java/com/example/workmanager/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.workmanager;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(MyWorker.TAG, "onCreate " + Thread.currentThread().getName());
+        doWork();
+    }
+
+    public void doWork() {
+        PeriodicWorkRequest request = new PeriodicWorkRequest.Builder(MyWorker.class, 1, TimeUnit.MINUTES, 5, TimeUnit.MINUTES).build();
+        WorkManager.getInstance().enqueueUniquePeriodicWork("Refresh News", ExistingPeriodicWorkPolicy.REPLACE, request);
+        LiveData<WorkInfo> info = WorkManager.getInstance().getWorkInfoByIdLiveData(request.getId());
+        info.observe(this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(@Nullable WorkInfo workInfo) {
+                Log.d(MyWorker.TAG, "onChanged " + Thread.currentThread().getName() + " " + workInfo.getState());
+            }
+        });
     }
 }
+
+
--- workmanager/src/main/java/com/example/workmanager/MyWorker.java
+++ workmanager/src/main/java/com/example/workmanager/MyWorker.java
@@ -1,7 +1,34 @@
 package com.example.workmanager;
-import androidx.
 
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import java.util.concurrent.TimeUnit;
 
 public class MyWorker extends Worker {
+    public static final String TAG = "myLogsUniq4";
 
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.d(TAG, "doWork: start " + Thread.currentThread().getName());
+
+        try {
+            TimeUnit.SECONDS.sleep(4);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "doWork: end " + Thread.currentThread().getName());
+        return Result.success();
+    }
 }
+
